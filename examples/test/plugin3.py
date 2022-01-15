6400b1db757fe0f8729d8e48017a81bc4399ad31
Rounak <62498036+Rounak-Das-02@users.noreply.github.com>
2022-01-15 18:12:53 +0530

    Added some examples for testing

--- examples/test/plugin3.py
+++ examples/test/plugin3.py
@@ -1,16 +1,27 @@
 import hikari
 import lightbulb
 
-info_plugin = lightbulb.Plugin("Info")
+info_plugin = lightbulb.Plugin("Info3")
 
 
 @info_plugin.command
-@lightbulb.command(
-    "hello", "Nothing"
-)
+@lightbulb.command("hel", "Nothing")
 @lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
-async def hello(ctx: lightbulb.Context) -> None:
-    await ctx.respond("hello")
+async def hel(ctx: lightbulb.Context) -> None:
+    await ctx.respond("hel")
+
+@info_plugin.command
+@lightbulb.command("hell", "Yo")
+@lightbulb.implements(lightbulb.PrefixCommand)
+async def hell(ctx : lightbulb.Context) -> None:
+    await ctx.respond("hell")
+
+@info_plugin.command
+@lightbulb.command("yo" , "YO")
+@lightbulb.implements(lightbulb.PrefixCommand)
+async def yoo(ctx : lightbulb.Context) -> None:
+    await ctx.respond("yo")
+
 
 def load(bot: lightbulb.BotApp) -> None:
     bot.add_plugin(info_plugin)
