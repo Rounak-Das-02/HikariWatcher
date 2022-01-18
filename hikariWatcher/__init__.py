320e2b5e04d05775ffa9e4046705b0dff440cd2b
Rounak <62498036+Rounak-Das-02@users.noreply.github.com>
2022-01-18 19:47:01 +0530

    Modified __init__ files

--- hikariWatcher/__init__.py
+++ hikariWatcher/__init__.py
@@ -1,17 +1,10 @@
-"""
-dpymenus -- Simplified menus for discord.py developers.
-"""
+from hikariWatcher.hikariWatch import Watcher, watch
 
 __title__ = "hikariWatcher"
 __author__ = "Rounak Das <dasrounak2002@gmail.com>"
 __license__ = "MIT"
 __copyright__ = "Copyright 2022-2023 Rounak Das"
-__version__ = "1.0.0"
-
-import logging
-
-from hikariWatcher.hikariWatch import Watcher, watch
+__version__ = "0.0.1"
 
 
-# logger = logging.getLogger("hikariWatcher")
-# logger.addHandler(logging.NullHandler())
+
